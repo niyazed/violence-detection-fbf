c8700f6221622057918ffd388c274c20e035313b
Niyaz B. Hashem <niyazed@outlook.com>
2019-03-03 17:24:42 +0600

    new data added in npy form

--- input_lstm.py
+++ input_lstm.py
@@ -17,11 +17,11 @@ labels=['violent','non_violent']
 
 # Train dataset loading and Stacking both label
 
-X_train = np.load('C:/Users/NBH/Desktop/violence-detection/hockey_32x32_10FPS/train/'+labels[0] + '.npy')
+X_train = np.load('C:/Users/NBH/Desktop/violence-detection/32x32_10FPS/train/'+labels[0] + '.npy')
 y_train = np.zeros(X_train.shape[0])
 
 for i, label in enumerate(labels[1:]):
-    x = np.load('C:/Users/NBH/Desktop/violence-detection/hockey_32x32_10FPS/train/'+label + '.npy')
+    x = np.load('C:/Users/NBH/Desktop/violence-detection/32x32_10FPS/train/'+label + '.npy')
     X_train = np.vstack((X_train, x))
     y_train = np.append(y_train, np.full(x.shape[0], fill_value= (i + 1)))
 
@@ -29,11 +29,11 @@ for i, label in enumerate(labels[1:]):
 
 # Test dataset loading and  Stacking both label
 
-X_test = np.load('C:/Users/NBH/Desktop/violence-detection/hockey_32x32_10FPS/test/'+labels[0] + '.npy')
+X_test = np.load('C:/Users/NBH/Desktop/violence-detection/32x32_10FPS/test/'+labels[0] + '.npy')
 y_test = np.zeros(X_test.shape[0])
 
 for i, label in enumerate(labels[1:]):
-    x = np.load('C:/Users/NBH/Desktop/violence-detection/hockey_32x32_10FPS/test/'+label + '.npy')
+    x = np.load('C:/Users/NBH/Desktop/violence-detection/32x32_10FPS/test/'+label + '.npy')
     X_test = np.vstack((X_test, x))
     y_test = np.append(y_test, np.full(x.shape[0], fill_value= (i + 1)))
 
@@ -44,16 +44,22 @@ layer = Conv1D(1,3, kernel_regularizer=regularizers.l2(0.01))(inp)
 layer = BatchNormalization()(layer)
 layer = Activation('relu') (layer) 
 
-# layer = Conv1D(1,3, kernel_regularizer=regularizers.l2(0.01))(layer)
-# layer = BatchNormalization()(layer)
-# layer = Activation('relu') (layer)
+layer = Conv1D(1,3, kernel_regularizer=regularizers.l2(0.01))(layer)
+layer = BatchNormalization()(layer)
+layer = Activation('relu') (layer)
 
+layer = Conv1D(1,3, kernel_regularizer=regularizers.l2(0.01))(layer)
+layer = BatchNormalization()(layer)
+layer = Activation('relu') (layer)
 
-layer = CuDNNLSTM(200,return_sequences=False, kernel_regularizer=regularizers.l2(0.01)) (layer)
+layer = CuDNNLSTM(200,return_sequences=True, kernel_regularizer=regularizers.l2(0.01)) (layer)
 layer = Dropout(0.5)(layer)
 
-# layer = CuDNNLSTM(128, kernel_regularizer=regularizers.l2(0.01)) (layer)
-# layer = Dropout(0.5)(layer)
+layer = CuDNNLSTM(200,return_sequences=True, kernel_regularizer=regularizers.l2(0.01)) (layer)
+layer = Dropout(0.5)(layer)
+
+layer = CuDNNLSTM(200, kernel_regularizer=regularizers.l2(0.01)) (layer)
+layer = Dropout(0.5)(layer)
 
 
 layer = Dense(100)(layer)
